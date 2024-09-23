3f5095d81f1150382e8d59df9b23b1f7c6499cbb
yohanii <yohanii@github.com>
2024-09-23 17:25:27 +0900

    test: BaseballGame 테스트 작성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,26 @@
+package baseball;
+
+public class BaseballGame {
+
+    private Integer answer;
+
+    public BaseballGame() {
+        this.answer = 0;
+    }
+
+    public Integer getAnswer() {
+        return answer;
+    }
+
+    public int generateAnswer() {
+        return 0;
+    }
+
+    public String receiveUserInput() {
+        return null;
+    }
+
+    public boolean score(int answer, int userAnswer) {
+        return false;
+    }
+}
