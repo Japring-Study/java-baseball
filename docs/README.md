f762c222614507fa8af1f4dda5fb194bb847e2ba
yohanii <yohanii@github.com>
2024-09-23 19:53:52 +0900

    feat: 세 자리수 랜덤 숫자 생성 - generateAnswer()

--- docs/README.md
+++ docs/README.md
@@ -4,6 +4,6 @@
   - [ ] 게임 시작 - start()
   - [ ] 게임 종료시 재시작 기능 - restart()
 - [ ] 숫자 야구 구현 - BaseballGame class
-  - [ ] 세 자리수 랜덤 숫자 생성 - generateAnswer()
+  - [x] 세 자리수 랜덤 숫자 생성 - generateAnswer()
   - [ ] 사용자 입력 받기 - receiveUserInput()
   - [ ] 사용자 숫자를 받아 채점 및 정답 여부 반환 - score()
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class BaseballGame {
 
     private Integer answer;
@@ -13,7 +19,17 @@ public class BaseballGame {
     }
 
     public int generateAnswer() {
-        return 0;
+
+        AtomicInteger index = new AtomicInteger();
+
+        answer = Randoms.shuffle(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9)).stream()
+                .limit(3)
+                .reduce(0, (x, y) -> {
+                    int result = (int) (x + y * Math.pow(10, index.doubleValue()));
+                    index.getAndIncrement();
+                    return result;
+                });
+        return answer;
     }
 
     public String receiveUserInput() {
