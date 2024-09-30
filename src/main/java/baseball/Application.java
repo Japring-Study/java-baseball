30e6b720592c8e62c0dc91d91ec12d37ba2d33c6
JOKER_SON <alreadysons@gmail.com>
2024-10-01 00:55:22 +0900

    Main 클래스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("숫자 야구 게임을 시작합니다.\n");
+            GameStart.start();
+        } catch (IllegalArgumentException e) {
+            // 예외가 발생하면 프로그램 종료
+        } catch (InvalidInputException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
